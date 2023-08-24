cdb849299d901bd5c1abe49a24138c9a2c3ce211
hadsome-Droid <xkumbhax@gmail.com>
2023-08-24 19:31:10 +0300

    Main part 2 complete

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -2,25 +2,48 @@ import React from 'react';
 import MyPhoto from '../../../assets/images/photo.webp'
 import styled from "styled-components";
 import {FlexWrapper} from "../../../components/FlexWrapper";
+import {Container} from "../../../components/Container";
+import {theme} from "../../../styles/Theme";
 
 export const Main = () => {
     return (
         <StyledMain>
-            <FlexWrapper align={'center'} justify={'space-around'}>
-                <div>
-                    <MainText>Hi There</MainText>
-                    <Name>I am Svetlana Dyablo</Name>
-                    <MainTitle>A Web Developer.</MainTitle>
-                </div>
+            <Container>
+                <FlexWrapper align={'center'} justify={'space-between'}>
+                    <div>
+                        <MainText>Hi There</MainText>
+                        <Name>I am <span>Svetlana Dyablo</span></Name>
+                        <MainTitle>A Web Developer.</MainTitle>
+                    </div>
+                    <PhotoWrapper>
+                        <Photo src={MyPhoto} alt={'photo'}/>
+                    </PhotoWrapper>
 
-                <Photo src={MyPhoto} alt={'photo'}/>
-            </FlexWrapper>
+                </FlexWrapper>
+            </Container>
         </StyledMain>
     );
 };
 
 const StyledMain = styled.section`
   min-height: 100vh;
+  display: flex;
+`
+const PhotoWrapper = styled.div`
+  position: relative;
+  z-index: 0;
+
+  &::before {
+    content: '';
+    width: 360px;
+    height: 470px;
+    border: 5px solid ${theme.colors.accent};
+
+    position: absolute;
+    top: -24px;
+    left: 24px;
+    z-index: -1;
+  }
 `
 
 const Photo = styled.img`
@@ -30,12 +53,36 @@ const Photo = styled.img`
 `
 
 const MainTitle = styled.h1`
-
+  font-size: 27px;
+  font-weight: 400;
 `
 const Name = styled.h2`
+  font-family: Josefin Sans, sans-serif;
+  font-size: 50px;
+  font-weight: 700;
+  letter-spacing: 2.5px;
+  margin: 10px 0;
 
+  span {
+    position: relative;
+    z-index: 0;
+
+    &::before {
+      content: '';
+      display: inline-block;
+      width: 100%;
+      height: 20px;
+      background-color: ${theme.colors.accent};
+
+
+      position: absolute;
+      bottom: 0;
+      z-index: -1;
+    }
+  }
 `
 
 const MainText = styled.span`
-
+  font-size: 14px;
+  font-weight: 400;
 `
