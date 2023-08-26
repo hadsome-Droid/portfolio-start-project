9c89dd7191b134759511c8ab60c40d63b37ea8b1
hadsome-Droid <xkumbhax@gmail.com>
2023-08-26 20:59:01 +0300

    Contacts block part 2 complete

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -2,32 +2,61 @@ import React from 'react';
 import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
 import {Button} from "../../../components/Button";
+import {Container} from "../../../components/Container";
+import {theme} from "../../../styles/Theme";
 
 export const Contact = () => {
     return (
         <StyledContacts>
-            <SectionTitle>Contact</SectionTitle>
-            <StyledForm>
-                <Field placeholder={'name'}/>
-                <Field placeholder={'subject'}/>
-                <Field as={'textarea'} placeholder={'message'}/>
-                <Button type={'submit'}>Send message</Button>
-            </StyledForm>
+            <Container>
+                <SectionTitle>Contact</SectionTitle>
+                <StyledForm>
+                    <Field placeholder={'name'}/>
+                    <Field placeholder={'subject'}/>
+                    <Field as={'textarea'} placeholder={'message'}/>
+                    <Button type={'submit'}>Send message</Button>
+                </StyledForm>
+            </Container>
         </StyledContacts>
     );
 };
 
 const StyledContacts = styled.section`
-  min-height: 60vh;
+
 `
+
 const StyledForm = styled.form`
-  max-width: 500px;
+  max-width: 540px;
   width: 100%;
   display: flex;
   flex-direction: column;
-  gap: 10px;
+  align-items: center;
+  gap: 16px;
   margin: 0 auto;
+
+  textarea {
+    resize: none;
+    height: 155px;
+  }
 `
 const Field = styled.input`
+  width: 100%;
+  border: 1px solid ${theme.colors.borderColor};
+  background-color: ${theme.colors.secondaryBg};
+  padding: 7px 15px;
 
+  color: ${theme.colors.font};
+  font-family: 'Poppins', sans-serif;
+  font-size: 12px;
+  font-weight: 400;
+  letter-spacing: 0.6px;
+
+  &::placeholder {
+    color: ${theme.colors.placeholderColor};
+    text-transform: capitalize;
+  }
+
+  &:focus-visible {
+    outline: 1px solid ${theme.colors.borderColor};
+  }
 `
