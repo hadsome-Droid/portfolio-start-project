4e858bcba6ef9a6cdde86c16c54ac2e7ef4d1896
hadsome-Droid <xkumbhax@gmail.com>
2023-08-26 21:27:00 +0300

    Footer block part 2 complete

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from "styled-components";
 import {Icon} from "../../components/icon/Icon";
 import {FlexWrapper} from "../../components/FlexWrapper";
+import {theme} from "../../styles/Theme";
 
 
 type FooterPropsType = {
@@ -28,22 +29,47 @@ export const Footer = (props: { footerIcon: Array<string> }) => {
 };
 
 const StyledFooter = styled.footer`
-  min-height: 20vh;
+  background-color: ${theme.colors.primaryBg};
+  padding: 40px 0;
 
 `
 const Name = styled.span`
-
+  font-family: 'Josefin Sans', sans-serif;
+  font-size: 22px;
+  font-weight: 700;
+  letter-spacing: 3px;
 `
 const SocialList = styled.ul`
   display: flex;
-  gap: 30px;
+  gap: 20px;
+  margin: 30px 0;
 `
 const SocialItem = styled.li`
 
 `
 const SocialLink = styled.a`
+  border-radius: 50%;
+  background: rgba(255, 255, 255, 0.10);
+  width: 35px;
+  height: 35px;
+  cursor: pointer;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  color: ${theme.colors.accent};
+
+  &:hover {
+    color: ${theme.colors.primaryBg};
+    transform: translateY(-4px);
+  }
+
 
 `
 const Copyright = styled.small`
-
+  text-align: center;
+  font-size: 12px;
+  font-weight: 400;
+  opacity: 0.5;
 `
