edf4ffeea9536a02f674cc413d64c2896138caf1
hadsome-Droid <xkumbhax@gmail.com>
2023-09-06 20:30:52 +0300

    React-parallax-tilt complete

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -4,6 +4,7 @@ import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Container} from "../../../components/Container";
 import {S} from "./Main_Styles"
 import Typewriter from 'typewriter-effect';
+import Tilt from 'react-parallax-tilt';
 
 export const Main: React.FC = () => {
     return (
@@ -13,7 +14,6 @@ export const Main: React.FC = () => {
                     <div>
                         <S.MainText>Hi There</S.MainText>
                         <S.Name>I am <span>Svetlana Dyablo</span></S.Name>
-                        {/*<S.MainTitle>A Web Developer.</S.MainTitle>*/}
                         <S.MainTitle>
                             <p>A Web Developer.</p>
                             <Typewriter
@@ -26,9 +26,16 @@ export const Main: React.FC = () => {
                             />
                         </S.MainTitle>
                     </div>
-                    <S.PhotoWrapper>
-                        <S.Photo src={MyPhoto} alt={'photo'}/>
-                    </S.PhotoWrapper>
+                    <Tilt
+                        className="parallax-effect-glare-scale"
+                        perspective={500}
+                        glareEnable={true}
+                        glareMaxOpacity={0.45}
+                        scale={1.02}>
+                        <S.PhotoWrapper>
+                            <S.Photo src={MyPhoto} alt={'photo'}/>
+                        </S.PhotoWrapper>
+                    </Tilt>
                 </FlexWrapper>
             </Container>
         </S.Main>
